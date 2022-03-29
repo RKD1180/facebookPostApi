0721a60054a72e58417eda995815ac3490dbe151
1emonislam <emonislamweb@gmail.com>
2022-03-29 20:58:18 +0600

    graph api

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,12 +1,68 @@
-import React from "react";
+import { Alert, CircularProgress } from "@mui/material";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-
+import React, { useState } from "react";
+import { useForm } from 'react-hook-form';
 const Home = () => {
+  const { register, reset, handleSubmit } = useForm();
+  const [error, setError] = useState("")
+  const [success, setSuccess] = useState("")
+  const [url, setUrl] = useState("");
+  const [loading, setLoading] = useState(false)
+  // console.log(image)
+  const uploadImage = (e) => {
+    // console.log('click')
+    setLoading(true)
+    const image = e.target.files[0];
+    // console.log(image)
+    const data = new FormData();
+    data.append("file", image);
+    data.append("upload_preset", "donor-photo-uploads");
+    data.append("cloud_name", "dydja7ouv");
+    fetch("https://api.cloudinary.com/v1_1/dydja7ouv/image/upload", {
+      method: "post",
+      body: data
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setUrl(data.url);
+        setLoading(false)
+      });
+  };
+  // console.log(process.env.REACT_APP_ACCESS_TOKEN)
+
+  const onSubmit = data => {
+    if (url) data.img = url;
+    if (url) {
+      fetch(`https://graph.facebook.com/v13.0/${process.env.REACT_APP_PAGE_ID}/photos?url=${data?.img}&message=${data?.text}&access_token=${process.env.REACT_APP_ACCESS_TOKEN}`)
+        .then(res => res.json())
+        .then(data => {
+          if (data.error) {
+            setError(data?.error?.message)
+          }
+          if (data?.data) {
+            reset()
+            setSuccess('You have successfully added facebook posts')
+          }
+        })
+    } else {
+      fetch(`https://graph.facebook.com/v13.0/${process.env.REACT_APP_PAGE_ID}/feed?message=${data?.text}&access_token=${process.env.REACT_APP_ACCESS_TOKEN}`)
+        .then(res => res.json())
+        .then(data => {
+          if (data.error) {
+            setError(data?.error?.message)
+          }
+          if (data?.data) {
+            reset()
+            setSuccess('You have successfully added facebook posts')
+          }
+        })
+    }
+  }
   return (
     <div className="form">
-      <form action="">
+      <form onSubmit={handleSubmit(onSubmit)}>
         <Box
           sx={{
             width: 500,
@@ -15,24 +71,33 @@ const Home = () => {
             marginTop: 10,
           }}
         >
+          {url ? <img src={url} width="100px" height="100px" alt="" /> : ''
+          }
+          {loading ? <CircularProgress /> : !url && <Button sx={{ mt: 3 }} variant="contained" component="label">
+            Upload Images
+            <input type="file" hidden onChange={uploadImage} />
+          </Button>}
+          <br />
+          <br />
           <TextField
             fullWidth
             multiline
             rows={4}
-            maxRows={4}
+            maxrows={4}
             label="text"
             id="fullWidth"
+            {...register("text", { min: 0 })} required
           />
-          <Button sx={{ mt: 3 }} variant="contained" component="label">
-            Upload File
-            <input type="file" hidden />
-          </Button>{" "}
-          <br />
-          <Button sx={{ mt: 2 }} variant="contained">
+
+          <Button type="submit" sx={{ mt: 2 }} variant="contained">
             Submit
           </Button>
         </Box>
       </form>
+      <br />
+
+      {error && <Alert severity="error" autoHideDuration={5000} style={{ textAlign: 'center', width: '80%', margin: '0 auto' }}>{error}</Alert>}
+      {success && <Alert severity="success" autoHideDuration={5000} style={{ textAlign: 'center', width: '80%', margin: '0 auto' }}>{success}</Alert>}
     </div>
   );
 };
